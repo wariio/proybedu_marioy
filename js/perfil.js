90f61ea9fc1c45a254b4c4eed0fc741cd6997459
digital <acanarte@LT1131134ILMAC.local>
2019-05-10 19:01:05 -0500

    initial commit

---
+++ js/perfil.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$(".p1").click(function(){
+		$(".panel-heading").removeClass("activo");
+		$(".panel-1").addClass("activo");
+	});
+
+	$(".p2").click(function(){
+		$(".panel-heading").removeClass("activo");
+		$(".panel-2").addClass("activo");
+	});
+
+	$(".p3").click(function(){
+		$(".panel-heading").removeClass("activo");
+		$(".panel-3").addClass("activo");
+	});
+
+	$(".p4").click(function(){
+		$(".panel-heading").removeClass("activo");
+		$(".panel-4").addClass("activo");
+	});
+	
+});
